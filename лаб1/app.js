b407363f29fda3e7e86ba18277f2a957b9688ad5
Valikosha <eliteruss04@gmail.com>
2025-11-05 15:08:10 +0300

    lab1

---
+++ лаб1/app.js
@@ -0,0 +1,38 @@
+// Функция для добавления числа или оператора в экран
+function addToInput(value) {
+    var input = document.getElementById('input'); // Получаем элемент ввода по ID 'input' и приводим его к типу HTMLInputElement
+    input.value += value; // Добавляем новое значение (число или оператор) к текущему значению строки
+}
+// Функция для очистки ввода
+function clearInput() {
+    var input = document.getElementById('input'); // Получаем элемент ввода по ID 'input'
+    input.value = ''; // Устанавливаем пустую строку, тем самым очищая ввод
+}
+// Чистая функция для выполнения математических операций
+function evaluateExpression(expression) {
+    // Заменяем символ '^' на '**' для возведения в степень в JavaScript
+    expression = expression.replace(/\^/g, '**');
+    try {
+        return new Function('return ' + expression)(); // Динамически вычисляем выражение из строки и возвращаем результат
+    }
+    catch (error) {
+        return NaN; // Если в выражении ошибка (например, некорректные символы), возвращаем NaN
+    }
+}
+// Функция для вычисления результата
+function calculate() {
+    var input = document.getElementById('input'); // Получаем элемент ввода по ID 'input'
+    var result = evaluateExpression(input.value); // Передаем текущее выражение в поле ввода в функцию evaluateExpression для вычисления
+    input.value = result.toString(); // Преобразуем результат в строку и отображаем его в поле ввода
+}
+// Новая функция для вычисления квадратного корня
+function squareRoot() {
+    var input = document.getElementById('input'); // Получаем элемент ввода по ID 'input'
+    var value = parseFloat(input.value); // Преобразуем строку ввода в число с плавающей точкой
+    if (!isNaN(value)) { // Проверяем, является ли преобразованное значение корректным числом
+        input.value = Math.sqrt(value).toString(); // Вычисляем квадратный корень числа и преобразуем результат в строку
+    }
+    else {
+        input.value = 'Ошибка'; // Если значение не является числом, отображаем сообщение об ошибке
+    }
+}
